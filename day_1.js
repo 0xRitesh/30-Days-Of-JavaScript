834b334f8db1b6c61b68fc81c305eb6e84eb2107
Ritesh Kumar <riteshkumar.js@protonmail.com>
2022-08-24 00:57:18 +0530

    Day 1 assignment : Introduction

---
+++ day_1.js
@@ -0,0 +1,39 @@
+// Write a single line comment which says, comments can make code readable
+
+ // comments can make code readable
+
+// Write another single comment which says, Welcome to 30DaysOfJavaScript
+
+// Welcome to 30DaysOfJavaScript
+
+// Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+
+  /* 
+  comments can make code readable, 
+  easy to reuse and informative 
+  */
+
+// Declare four variables without assigning values
+
+let a, b,c,d;
+
+// Declare four variables with assigned values
+
+let e = 10, f = 'Hi',g = 11, h = 12;
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+let first_name = 'Ritesh';
+let last_name = 'kumar';
+let marital_status = 'unmarried';
+let country = 'India';
+var age = 20;
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+
+let first_name1 = 'Ritesh', last_name1 = 'kumar', marital_status1 = 'unmarried', country1 = 'India', age1 = 20;
+
+// Declare a variable named community and assign them initial value of 'Pesto' and log to the browser console a message "Hello Pesto"
+
+community = 'Hello Pesto'
+console.log(community)
