4d906891a57d435bd4d58c0e3d0c9dfee4a03807
Ritesh Kumar <riteshkumar.js@protonmail.com>
2022-09-11 11:57:16 +0530

    Day 13: started with Level 1

---
+++ Day-13/Day-13-level-1.js
@@ -0,0 +1,37 @@
+// Exercises:Level 1
+// Display the countries array as a table
+const countries = [
+    ['Finland', 'Helsinki'],
+    ['Sweden', 'Stockholm'],
+    ['Norway', 'Oslo']
+]
+console.table(countries)
+
+// Display the countries object as a table
+// Use console.group() to group logs
+
+let countries = [{
+        name: "nigeria",
+        capital: "fct",
+        population: "7billion",
+        languages: ["hausa", "igbo", "yoruba"]
+    },
+    {
+        name: "germany",
+        capital: "munich",
+        population: "5billion",
+        languages: ["german"]
+    }, {
+        name: "italy",
+        capital: "rome",
+        population: "6billion",
+        languages: ["italian", "latin"]
+    },
+    {
+        name: "france",
+        capital: "paris",
+        population: "6billion",
+        languages: ["french"]
+    }
+]
+console.table(countries)
