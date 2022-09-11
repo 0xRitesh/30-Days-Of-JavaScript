e3bc917c107ce9d4a2db29259436a588de97e5b4
Ritesh Kumar <riteshkumar.js@protonmail.com>
2022-09-11 11:58:44 +0530

    Day 13: Level -2 cmplt

---
+++ Day-13-level-2.js
@@ -0,0 +1,15 @@
+// Exercises:Level 2
+// 10 > 2 * 10 use console.assert()
+
+//assertion message
+console.assert(10 > 2 * 10, "incorrect")
+
+// Write a warning message using console.warn()
+
+//warning message
+console.warn("i'm warning you")
+
+// Write an error message using console.error()
+
+//error message
+console.error("this is an error")
