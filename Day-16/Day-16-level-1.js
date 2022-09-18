d5bbde25b0beec8580984dd6b148bcec2338634e
Ritesh Kumar <riteshkumar.js@protonmail.com>
2022-09-18 23:03:56 +0530

    Day 16: done with level 1

--- Day-16/Day-16-level-1.js
+++ Day-16/Day-16-level-1.js
@@ -4,3 +4,23 @@
 // Stringify the isMarried variable
 // Stringify the student object
 
+const skills = ['HTML', 'CSS', 'JS', 'React', 'Node', 'Python']
+const skillsTxt = JSON.stringify(skills)
+console.log(skillsTxt);
+
+let age = 250;
+const ageTxt = JSON.stringify(age)
+console.log(ageTxt)
+
+let isMarried = true
+const isMTxt = JSON.stringify(isMarried)
+console.log(isMTxt);
+
+const student = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 250,
+    isMarried: true,
+    skills: ['HTML', 'CSS', 'JS', 'React', 'Node', 'Python', ]
+}
+let studentTxt = JSON.stringify(student)
