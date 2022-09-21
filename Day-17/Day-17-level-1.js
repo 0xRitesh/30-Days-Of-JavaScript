70c90dacb12137e7460df776888dbf65711f79a2
Ritesh Kumar <riteshkumar.js@protonmail.com>
2022-09-21 22:24:28 +0530

    Day-17: completed Level -1

--- Day-17/Day-17-level-1.js
+++ Day-17/Day-17-level-1.js
@@ -1,3 +1,13 @@
 // Exercises: Level 1
 // Store you first name, last name, age, country, city in your browser localStorage.
 
+let profile = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 20,
+    country: "US",
+    city: "CA",
+}
+
+let txt = JSON.stringify(profile);
+localStorage.setItem("user", "txt");
