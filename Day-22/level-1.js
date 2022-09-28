328c36b3781f6eb87eca54fbd0c110341d8c479b
Ritesh Kumar <riteshkumar.js@protonmail.com>
2022-09-28 14:08:29 +0530

    Day 22: started with level 1

---
+++ Day-22/level-1.js
@@ -0,0 +1,8 @@
+// Exercises: Level 1
+// Create a div container on HTML document and create 100 to 100 numbers dynamically and append to the container div.
+// Even numbers background is green
+// Odd numbers background is yellow
+// Prime numbers background is red
+// Number Generator
+
+// https://github.com/Asabeneh/30-Days-Of-JavaScript/blob/master/22_Day_Manipulating_DOM_object/22_day_manipulating_DOM_object.md#exercises-level-1
